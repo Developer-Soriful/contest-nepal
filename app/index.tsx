3c75e9b99b50dff3c494d38445dbb2b27a4f4f5f
Developer-Soriful <93891396soriful@gmail.com>
2026-03-12 23:31:56 +0600

    i have resolve the initiall screen .

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { import_img } from "@/assets/import_img";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const handleGetStarted = () => {
-    router.navigate("/login")
-  }
+    router.navigate("/login");
+  };
   return (
     <SafeAreaView
       style={{
@@ -15,14 +16,23 @@ export default function Index() {
       }}
     >
       {/* app logos */}
-      <View style={{ position: "absolute", top: 0, left: 0, right: 0, padding: 20, zIndex: 10 }}>
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          padding: 20,
+          zIndex: 10,
+        }}
+      >
         <Image source={import_img.icon} style={{ width: 100, height: 100 }} />
       </View>
       {/* middle image */}
       <View
         style={{
           position: "absolute",
-          top: 100,
+          top: 120,
           left: 0,
           right: 0,
           height: 350,
@@ -38,28 +48,34 @@ export default function Index() {
             justifyContent: "flex-start",
             alignItems: "flex-start",
             gap: 10,
-            top: -50
+            top: -50,
           }}
         >
-          <Text style={{
-            fontSize: 34,
-            fontWeight: 600,
-            color: "white"
-          }}>Smart
-            Play Wins</Text>
-          <Text style={{
-            fontSize: 14,
-            fontWeight: 500,
-            color: "white"
-          }}>
-            Learn fast, think smart.
-            win every challenge.
+          <Text
+            style={{
+              fontSize: 26,
+              fontWeight: 600,
+              color: "white",
+              width: "100%",
+            }}
+          >
+            Smart Play Wins
+          </Text>
+          <Text
+            style={{
+              fontSize: 12,
+              fontWeight: 500,
+              color: "white",
+            }}
+          >
+            Learn fast, think smart. win every challenge.
           </Text>
         </View>
         <Image
           source={import_img.bottom_squre}
           style={{
             position: "absolute",
+            top: 0,
             width: 300,
             height: 300,
             transform: [{ rotate: "-15deg" }],
@@ -102,19 +118,99 @@ export default function Index() {
           flexDirection: "column",
           gap: 16,
           justifyContent: "center",
-          alignItems: "center"
+          alignItems: "center",
         }}
       >
-        <Text style={{ fontSize: 32, fontWeight: "bold", textAlign: "center", lineHeight: 45, letterSpacing: -0.5 }}>Participate Smart Contest Every Day</Text>
-        <Text style={{ fontSize: 18, color: "#323232", textAlign: "center", lineHeight: 26 }}>
-          Boost knowledge daily, win challenges,
-          become smarter every day.
+        <Text
+          style={{
+            fontSize: 26,
+            fontWeight: "bold",
+            textAlign: "center",
+            lineHeight: 45,
+            letterSpacing: -0.5,
+          }}
+        >
+          Participate Smart Contest Every Day
         </Text>
-        <View style={{ marginTop: 10, width: "100%", borderColor: "#990109", borderWidth: 1, borderRadius: 50 }}>
-          <TouchableOpacity onPress={handleGetStarted} style={{ backgroundColor: "#990109", width: "100%", paddingVertical: 15, paddingHorizontal: 30, borderRadius: 50, borderColor: "white", borderWidth: 1 }}>
-            <Text style={{ color: "#fff", fontSize: 18, fontWeight: "bold", textAlign: "center" }}>
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#323232",
+            textAlign: "center",
+            lineHeight: 26,
+          }}
+        >
+          Boost knowledge daily, win challenges, become smarter every day.
+        </Text>
+        <View
+          style={{
+            marginTop: 10,
+            width: "100%",
+            borderColor: "#990109",
+            borderWidth: 1,
+            borderRadius: 50,
+          }}
+        >
+          <TouchableOpacity
+            onPress={handleGetStarted}
+            style={{
+              backgroundColor: "#990109",
+              width: "100%",
+              padding: 8,
+              borderRadius: 50,
+              borderColor: "white",
+              borderWidth: 1,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              gap: 10,
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "white",
+                borderRadius: "100%",
+                width: 40,
+                height: 40,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <FontAwesome name="plane" size={24} color="#990109" />
+            </View>
+            <Text
+              style={{
+                color: "#fff",
+                fontSize: 14,
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
               Get Started
             </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <MaterialIcons
+                name="keyboard-arrow-right"
+                size={11}
+                color="#ad333a"
+              />
+              <MaterialIcons
+                name="keyboard-arrow-right"
+                size={11}
+                color="#cc8084"
+              />
+              <MaterialIcons
+                name="keyboard-arrow-right"
+                size={11}
+                color="white"
+              />
+            </View>
           </TouchableOpacity>
         </View>
       </View>
