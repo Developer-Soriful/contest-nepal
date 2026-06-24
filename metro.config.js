6e6a4eb5da4c246a604cd4428057932d78c67911
Developer-Soriful <93891396soriful@gmail.com>
2026-06-25 04:44:13 +0600

    fix: override react-native to 0.81.5 and block nested dependency to resolve codegen event type incompatibility

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
@@ -6,5 +7,21 @@ config.resolver.alias = {
   ...(config.resolver.alias || {}),
   '@': './',
 };
+
+// Block the nested react-native@0.86.0 that npm incorrectly installs as a
+// peer dependency of @react-native/virtualized-lists@0.81.5. That version
+// contains VirtualViewExperimentalNativeComponent.js whose complex Flow event
+// type is incompatible with the @react-native/codegen@0.81.5 parser,
+// causing "Unable to determine event arguments for onModeChange".
+const nestedRNPath = path.join(
+  __dirname,
+  'node_modules',
+  'react-native',
+  'node_modules',
+  'react-native'
+);
+config.resolver.blockList = [
+  new RegExp(`^${nestedRNPath.replace(/\\/g, '\\\\')}.*`),
+];
 
 module.exports = config;
